f2cfcafefdb68b108319953046603ecfe1c5bed8
Nandan-unni <asnqln@gmail.com>
2021-05-09 19:46:08 +0530

    deploy:err 01

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('~/RetweetBot')
+load_dotenv(os.path.join(project_folder, '.env'))
+
 import sys
 path = '/home/sahaay/Sahaay-Bot'
 if path not in sys.path:
